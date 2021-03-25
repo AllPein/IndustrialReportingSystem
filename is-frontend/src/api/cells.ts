000d2dd23dc4dd2a366563de2825c61b0816d335
panin-ao <panin-ao@itrev.ru>
2021-03-25 13:10:08 +0500

    Обновление ячеек и оборудования

--- is-frontend/src/api/cells.ts
+++ is-frontend/src/api/cells.ts
@@ -3,13 +3,7 @@ import { api } from './index';
 
 export const fetchAllCells = async () => {
   const data = await api.get('/v1/cell');
-
   return data.data;
 }
 
 
-export const updateCells = async (data: Partial<Cell>[]) => {
-  const response = await api.patch('/v1/cell', data);
-
-  return response.data;
-}
